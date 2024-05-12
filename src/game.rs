c4f132d5bcb242da4a19ed56d66cc3cf5bcca9f5
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-05-12 16:52:31 +0900

    slideのアニメーションが再生されるところまでやった

---
+++ src/game.rs
@@ -0,0 +1,328 @@
+use self::red_hat_boy_states::*;
+use crate::engine::{Game, KeyState, Point, Rect, Renderer};
+use crate::{browser, engine};
+use anyhow::{anyhow, Result};
+use async_trait::async_trait;
+use serde::Deserialize;
+use std::collections::HashMap;
+use web_sys::HtmlImageElement;
+
+#[derive(Deserialize, Clone)]
+struct SheetRect {
+    x: i16,
+    y: i16,
+    w: i16,
+    h: i16,
+}
+
+#[derive(Deserialize, Clone)]
+struct Cell {
+    frame: SheetRect,
+}
+
+#[derive(Deserialize, Clone)]
+pub struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+pub struct WalkTheDog {
+    rhb: Option<RedHatBoy>,
+}
+
+#[async_trait(?Send)]
+impl Game for WalkTheDog {
+    async fn initialize(&self) -> Result<Box<dyn Game>> {
+        let sheet: Option<Sheet> = browser::fetch_json("rhb.json").await?.into_serde()?;
+
+        let image = Some(engine::load_image("rhb.png").await?);
+
+        Ok(Box::new(WalkTheDog {
+            rhb: Some(RedHatBoy::new(
+                sheet.clone().ok_or_else(|| anyhow!("No Sheet Present"))?,
+                image.clone().ok_or_else(|| anyhow!("No Image Present"))?,
+            )),
+        }))
+    }
+
+    fn update(&mut self, key_state: &KeyState) {
+        let mut velocity = Point { x: 0, y: 0 };
+        if key_state.is_pressed("ArrowDown") {
+            velocity.y += 3;
+            self.rhb.as_mut().unwrap().slide();
+        }
+
+        if key_state.is_pressed("ArrowUp") {
+            velocity.y -= 3
+        }
+
+        if key_state.is_pressed("ArrowRight") {
+            velocity.x += 3;
+            self.rhb.as_mut().unwrap().run_right();
+        }
+
+        if key_state.is_pressed("ArrowLeft") {
+            velocity.x -= 3
+        }
+
+        self.rhb.as_mut().unwrap().update();
+    }
+
+    fn draw(&self, renderer: &Renderer) {
+        renderer.clear(&Rect {
+            x: 0.0,
+            y: 0.0,
+            width: 600.0,
+            height: 600.0,
+        });
+
+        self.rhb.as_ref().unwrap().draw(renderer);
+    }
+}
+
+impl WalkTheDog {
+    pub fn new() -> Self {
+        WalkTheDog { rhb: None }
+    }
+}
+
+struct RedHatBoy {
+    state_machine: RedHatBoyStateMachine,
+    sprite_sheet: Sheet,
+    image: HtmlImageElement,
+}
+
+impl RedHatBoy {
+    fn new(sheet: Sheet, image: HtmlImageElement) -> Self {
+        Self {
+            state_machine: RedHatBoyStateMachine::Idle(RedHatBoyState::new()),
+            sprite_sheet: sheet,
+            image,
+        }
+    }
+
+    fn draw(&self, renderer: &Renderer) {
+        let frame_name = format!(
+            "{} ({}).png",
+            self.state_machine.frame_name(),
+            (self.state_machine.context().frame / 3) + 1
+        );
+
+        let sprite = self
+            .sprite_sheet
+            .frames
+            .get(&frame_name)
+            .expect("Cell not found");
+
+        renderer.draw_image(
+            &self.image,
+            &Rect {
+                x: sprite.frame.x.into(),
+                y: sprite.frame.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+            &Rect {
+                x: self.state_machine.context().position.x.into(),
+                y: self.state_machine.context().position.y.into(),
+                width: sprite.frame.w.into(),
+                height: sprite.frame.h.into(),
+            },
+        );
+    }
+
+    fn update(&mut self) {
+        self.state_machine = self.state_machine.update();
+    }
+
+    fn run_right(&mut self) {
+        self.state_machine = self.state_machine.transition(Event::Run);
+    }
+
+    fn slide(&mut self) {
+        self.state_machine = self.state_machine.transition(Event::Slide);
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum RedHatBoyStateMachine {
+    Idle(RedHatBoyState<Idle>),
+    Running(RedHatBoyState<Running>),
+    Sliding(RedHatBoyState<Sliding>),
+}
+
+impl From<RedHatBoyState<Running>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Running>) -> Self {
+        RedHatBoyStateMachine::Running(state)
+    }
+}
+
+impl From<RedHatBoyState<Sliding>> for RedHatBoyStateMachine {
+    fn from(state: RedHatBoyState<Sliding>) -> Self {
+        RedHatBoyStateMachine::Sliding(state)
+    }
+}
+
+impl RedHatBoyStateMachine {
+    fn transition(self, event: Event) -> Self {
+        match (self, event) {
+            (RedHatBoyStateMachine::Idle(state), Event::Run) => state.run().into(),
+            (RedHatBoyStateMachine::Running(state), Event::Slide) => state.slide().into(),
+            _ => self,
+        }
+    }
+
+    fn frame_name(&self) -> &str {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => state.frame_name(),
+            RedHatBoyStateMachine::Running(state) => state.frame_name(),
+            RedHatBoyStateMachine::Sliding(state) => state.frame_name(),
+        }
+    }
+
+    fn context(&self) -> &RedHatBoyContext {
+        match self {
+            RedHatBoyStateMachine::Idle(state) => &state.context(),
+            RedHatBoyStateMachine::Running(state) => &state.context(),
+            RedHatBoyStateMachine::Sliding(state) => &state.context(),
+        }
+    }
+
+    fn update(self) -> Self {
+        match self {
+            RedHatBoyStateMachine::Idle(mut state) => {
+                state.update();
+                RedHatBoyStateMachine::Idle(state)
+            }
+            RedHatBoyStateMachine::Running(mut state) => {
+                state.update();
+                RedHatBoyStateMachine::Running(state)
+            }
+            RedHatBoyStateMachine::Sliding(mut state) => {
+                state.update();
+                RedHatBoyStateMachine::Sliding(state)
+            }
+        }
+    }
+}
+
+// 専用モジュールに入れることで、gameモジュールの他の部分が必要とするメソッドだけ公開できる。
+mod red_hat_boy_states {
+    use crate::engine::Point;
+
+    const IDLE_FRAMES: u8 = 29;
+    const RUNNING_FRAMES: u8 = 23;
+    const SLIDING_FRAMES: u8 = 14;
+
+    const FLOOR: i16 = 475;
+    const RUNNING_SPEED: i16 = 3;
+    const IDLE_FRAME_NAME: &str = "Idle";
+    const RUN_FRAME_NAME: &str = "Run";
+    const SLIDING_FRAME_NAME: &str = "Slide";
+    #[derive(Copy, Clone)]
+    pub struct RedHatBoyState<S> {
+        context: RedHatBoyContext,
+        _state: S,
+    }
+
+    impl<S> RedHatBoyState<S> {
+        pub fn context(&self) -> &RedHatBoyContext {
+            &self.context
+        }
+    }
+
+    impl RedHatBoyState<Idle> {
+        pub fn new() -> Self {
+            RedHatBoyState {
+                context: RedHatBoyContext {
+                    frame: 0,
+                    position: Point { x: 0, y: FLOOR },
+                    velocity: Point { x: 0, y: 0 },
+                },
+                _state: Idle {},
+            }
+        }
+        pub fn run(self) -> RedHatBoyState<Running> {
+            RedHatBoyState {
+                context: self.context.reset_frame().run_right(),
+                _state: Running {},
+            }
+        }
+
+        pub fn frame_name(&self) -> &str {
+            IDLE_FRAME_NAME
+        }
+
+        pub fn update(&mut self) {
+            self.context = self.context.update(IDLE_FRAMES);
+        }
+    }
+
+    impl RedHatBoyState<Running> {
+        pub fn frame_name(&self) -> &str {
+            RUN_FRAME_NAME
+        }
+
+        pub fn update(&mut self) {
+            self.context = self.context.update(RUNNING_FRAMES);
+        }
+
+        pub fn slide(self) -> RedHatBoyState<Sliding> {
+            RedHatBoyState {
+                context: self.context.reset_frame(),
+                _state: Sliding {},
+            }
+        }
+    }
+
+    impl RedHatBoyState<Sliding> {
+        pub fn frame_name(&self) -> &str {
+            SLIDING_FRAME_NAME
+        }
+
+        pub fn update(&mut self) {
+            self.context = self.context.update(SLIDING_FRAMES);
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct RedHatBoyContext {
+        pub frame: u8,
+        pub position: Point,
+        pub velocity: Point,
+    }
+
+    impl RedHatBoyContext {
+        pub fn update(mut self, frame_count: u8) -> Self {
+            if self.frame < frame_count {
+                self.frame += 1;
+            } else {
+                self.frame = 0;
+            }
+            self.position.x += self.velocity.x;
+            self.position.y += self.velocity.y;
+            self
+        }
+        fn reset_frame(mut self) -> Self {
+            self.frame = 0;
+            self
+        }
+        fn run_right(mut self) -> Self {
+            self.velocity.x += RUNNING_SPEED;
+            self
+        }
+    }
+
+    pub enum Event {
+        Run,
+        Slide,
+    }
+
+    #[derive(Copy, Clone)]
+    pub struct Idle;
+
+    #[derive(Copy, Clone)]
+    pub struct Running;
+
+    #[derive(Copy, Clone)]
+    pub struct Sliding;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 mod browser;
 mod engine;
-use anyhow::{anyhow, Result};
+mod game;
+
+use crate::engine::GameLoop;
+use crate::game::WalkTheDog;
+use anyhow::Result;
 use gloo_utils::format::JsValueSerdeExt;
-use rand::{thread_rng, Rng};
+use rand::Rng;
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::rc::Rc;
-use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 
 #[derive(Deserialize)]
@@ -33,67 +35,12 @@ pub fn main_js() -> Result<(), JsValue> {
     // This provides better error messages in debug mode.
     console_error_panic_hook::set_once();
 
-    let context = browser::context().expect("Could not get browser context");
-
     browser::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
+        let game = WalkTheDog::new();
+
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json")
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
-
-        let image = web_sys::HtmlImageElement::new().unwrap();
-
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-        });
-
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-
-        image.set_src("rhb.png");
-        success_rx.await;
-
-        let mut frame = -1;
-        let internal_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            let sprite = sheet.frames.get(&frame_name).expect("Call not found");
-            context
-                .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                    &image,
-                    sprite.frame.x.into(),
-                    sprite.frame.y.into(),
-                    sprite.frame.w.into(),
-                    sprite.frame.h.into(),
-                    300.0,
-                    300.0,
-                    sprite.frame.w.into(),
-                    sprite.frame.h.into(),
-                )
-                .expect("could not draw Run (1).png");
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                internal_callback.as_ref().unchecked_ref(),
-                50,
-            );
-        internal_callback.forget();
+            .expect("Could not start game loop");
     });
 
     Ok(())
