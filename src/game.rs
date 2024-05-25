25c19951d40abc21cdf575f4371aec52341b6f5f
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-05-25 18:07:38 +0900

    6章終了

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,12 @@
 use anyhow::{anyhow, Result};
 use async_trait::async_trait;
+use rand::{thread_rng, Rng};
 use std::rc::Rc;
 use web_sys::HtmlImageElement;
 
 use self::red_hat_boy_states::*;
 use crate::engine::SpriteSheet;
-use crate::segments::stone_and_platform;
+use crate::segments::{platform_and_stone, stone_and_platform};
 use crate::{
     browser,
     engine::{self, Cell, Game, Image, KeyState, Point, Rect, Renderer, Sheet},
@@ -694,6 +695,27 @@ impl Walk {
     fn velocity(&self) -> i16 {
         -self.boy.walking_speed()
     }
+
+    fn generate_next_segment(&mut self) {
+        let mut rng = thread_rng();
+        let next_segment = rng.gen_range(0..2);
+        let mut next_obstacles = match next_segment {
+            0 => stone_and_platform(
+                self.stone.clone(),
+                Rc::clone(&self.obstacle_sheet),
+                self.timeline + OBSTACLE_BUFFER,
+            ),
+            1 => platform_and_stone(
+                self.stone.clone(),
+                Rc::clone(&self.obstacle_sheet),
+                self.timeline + OBSTACLE_BUFFER,
+            ),
+            _ => vec![],
+        };
+
+        self.timeline = rightmost(&next_obstacles);
+        self.obstacles.append(&mut next_obstacles);
+    }
 }
 
 impl WalkTheDog {
@@ -783,14 +805,7 @@ impl Game for WalkTheDog {
             });
 
             if walk.timeline < TIMELINE_MINIMUM {
-                let mut next_obstacles = stone_and_platform(
-                    walk.stone.clone(),
-                    Rc::clone(&walk.obstacle_sheet),
-                    walk.timeline + OBSTACLE_BUFFER,
-                );
-
-                walk.timeline = rightmost(&next_obstacles);
-                walk.obstacles.append(&mut next_obstacles);
+                walk.generate_next_segment()
             } else {
                 walk.timeline += velocity;
             }
