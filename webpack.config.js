cdc0abb184738514fe3c5c04ea5d7e8ae26b86c2
Stead08 <62312581+Stead08@users.noreply.github.com.>
2024-06-02 16:41:16 +0900

    update: webpack 4 to 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const test = require("node:test");
 
 const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
-  mode: "production",
-  entry: {
-    index: "./js/index.js"
-  },
-  output: {
-    path: dist,
-    filename: "[name].js"
-  },
-  devServer: {
-    contentBase: dist,
-  },
-  plugins: [
-    new CopyPlugin([
-      path.resolve(__dirname, "static")
-    ]),
+    mode: "production",
+    entry: "./js/index.js",
+    output: {
+        path: dist,
+        filename: "index.js"
+    },
+    devServer: {
+        static: {
+            directory: dist,
+        }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {to: dist, from: path.resolve(__dirname, "static")}
+            ]
+        }),
 
-    new WasmPackPlugin({
-      crateDirectory: __dirname,
-    }),
-  ]
+        new WasmPackPlugin({
+            crateDirectory: __dirname,
+        }),
+    ],
+    experiments: {
+        asyncWebAssembly: true,
+    },
+    performance: {
+        hints: process.env.NODE_ENV === 'production' ? "warning" : false
+    }
 };
